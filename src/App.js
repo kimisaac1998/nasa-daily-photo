ca04311fdbe9c0fc2e997c4ab85efa265f39d072
kimisaac1998 <kimisaac1998@gmail.com>
2022-06-21 23:12:04 -0700

    Add current day photo button and links

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import './App.css';
 import { Route, Routes, Link } from 'react-router-dom';
 import Home from './Home';
-import React, { useState, useEffect } from 'react';
 import NasaDetails from './NasaDetails';
+import NotFound from './NotFound';
 
 function App() {
 
 
+  const current = new Date();
+  const currentDate = `${current.getFullYear()}-${
+		current.getMonth() + 1
+		}-${current.getDate()}`;
+
+
+console.log(currentDate)
+
+
 	return (
 		<div className='App'>
-			
+			<nav>
+				<ul>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to={`/details/${currentDate}`}>Today's Photo</Link>
+					</li>
+				</ul>
+			</nav>
 
 			<Routes>
 				<Route path='/' element={<Home />} />
 
 				<Route path='/details/:date' element={<NasaDetails />} />
+				<Route path='*' element={<NotFound />} />
 			</Routes>
 		</div>
 	);
---
+++ src/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NotFound(props) {
+	return (
+		<div>
+			<h2>
+				Oops! Page not found. Click <Link to='/'>here</Link> to go back home.
+			</h2>
+			<img
+				src='https://media1.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif?cid=ecf05e47be0b97323b1715dd24c755f1d8d5e382314b0d3c&rid=giphy.gif&ct=g'
+				alt='404 Not Found'
+			/>
+		</div>
+	);
+}
+
+export default NotFound;

ae77a31b6718db0eda60c01a803dafc5e9fcd291
kimisaac1998 <kimisaac1998@gmail.com>
2022-06-23 14:40:20 -0700

    fix deploy

--- src/NasaDetails.js
+++ src/NasaDetails.js
@@ -10,19 +10,19 @@ function NasaDetails(props) {
 	const { date } = useParams();
 
 	const [nasa, setNasa] = useState(null);
+	const getUrl = () => {
+		return fetch(
+			`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${process.env.REACT_APP_IEX_KEY}`
+		)
+			.then((res) => res.json())
+			.then((res) => {
+				setNasa(res);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	};
 	useEffect(() => {
-		const getUrl = () => {
-			return fetch(
-				`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${process.env.REACT_APP_IEX_KEY}`
-			)
-				.then((res) => res.json())
-				.then((res) => {
-					setNasa(res);
-				})
-				.catch((err) => {
-					console.error(err);
-				});
-		};
 		getUrl();
 	}, []);
 
